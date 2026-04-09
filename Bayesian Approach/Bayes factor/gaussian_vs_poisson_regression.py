13759eb06825865343030a42b7797a40fc9e843c
Arya74phy <debanjanchatterjee74@gmail.com>
2026-04-09 19:57:00 +0530

    Update gaussian_vs_poisson_regression.py

--- Bayesian Approach/Bayes factor/gaussian_vs_poisson_regression.py
+++ Bayesian Approach/Bayes factor/gaussian_vs_poisson_regression.py
@@ -1,3 +1,5 @@
+from scipy.special import logsumexp
+
 degree = 2
 
 # Convert to counts
@@ -6,21 +8,25 @@ counts = np.round(100 * curr / np.mean(curr)).astype(int)
 counts[counts < 0] = 0
 curr_counts = counts
 
-# Gaussian likelihood
-logL_g = np.mean([
+# Gaussian likelihoods
+logL_g_samples = np.array([
     log_likelihood_gaussian(
         s, x, y, z, model_g.polynomial_model
     )
     for s in samples_g[:1000]
 ])
 
-# Poisson likelihood
-logL_p = np.mean([
+# Poisson likelihoods
+logL_p_samples = np.array([
     log_likelihood_poisson(
         s, x, y, curr_counts, model_p.polynomial_model
     )
     for s in samples_p[:1000]
 ])
+
+# Marginal likelihoods
+logL_g = logsumexp(logL_g_samples) - np.log(len(logL_g_samples))
+logL_p = logsumexp(logL_p_samples) - np.log(len(logL_p_samples))
 
 
 print("Gaussian sample length:", len(samples_g[0]))
