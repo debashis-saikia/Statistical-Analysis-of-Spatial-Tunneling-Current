b90c89d251035814cdce9a00587709d9bfb8f370
Arya74phy <debanjanchatterjee74@gmail.com>
2026-04-12 03:43:11 +0530

    Update poisson_model_mcmc_run_sampling.py

--- Bayesian Approach/Poisson Model/poisson_model_mcmc_run_sampling.py
+++ Bayesian Approach/Poisson Model/poisson_model_mcmc_run_sampling.py
@@ -11,10 +11,14 @@ for degree in range(1, 3):
 
     # Convert log-current → counts (Poisson requirement)
     curr = np.exp(z)
-
+    # Convert to counts
+    curr = np.exp(z)
+    counts = np.round(100 * curr / np.mean(curr)).astype(int)
+    counts[counts < 0] = 0
+    curr_counts = counts
     # --- Run MCMC ---
     samples_p = run_mcmc_poisson(
-        x, y, curr,
+        x, y, curr_counts,
         model.polynomial_model,
         degree,
         initial_beta=model.beta_hat
